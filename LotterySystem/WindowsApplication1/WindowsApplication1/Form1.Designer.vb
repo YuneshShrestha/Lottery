d94f9a2ce922b263e997dad87af626b1aeda0bd3
YuneshShrestha <52854563+YuneshShrestha@users.noreply.github.com>
2019-09-08 17:34:12 +1000

    Add files via upload

---
+++ LotterySystem/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,318 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtNumber2 = New System.Windows.Forms.TextBox()
+        Me.txtNumber1 = New System.Windows.Forms.TextBox()
+        Me.Panel8 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel7 = New System.Windows.Forms.Panel()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnLuckyDraw = New System.Windows.Forms.Button()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel6.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
+        Me.Panel8.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        Me.Panel7.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 15
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), System.Drawing.Image)
+        Me.Panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Controls.Add(Me.Panel3)
+        Me.Panel1.Controls.Add(Me.Panel8)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1135, 701)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Panel2.Controls.Add(Me.Panel6)
+        Me.Panel2.Controls.Add(Me.Panel4)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(1135, 122)
+        Me.Panel2.TabIndex = 7
+        '
+        'Panel6
+        '
+        Me.Panel6.BackColor = System.Drawing.Color.Transparent
+        Me.Panel6.Controls.Add(Me.Label4)
+        Me.Panel6.Controls.Add(Me.Label3)
+        Me.Panel6.Controls.Add(Me.Label2)
+        Me.Panel6.Location = New System.Drawing.Point(0, 0)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(561, 141)
+        Me.Panel6.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Mirarae BT", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Yellow
+        Me.Label4.Location = New System.Drawing.Point(367, 68)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 36)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "(+2)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Mirarae BT", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Yellow
+        Me.Label3.Location = New System.Drawing.Point(6, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(363, 36)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = " SECONDARY SCHOOL"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Mirarae BT", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Yellow
+        Me.Label2.Location = New System.Drawing.Point(6, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(381, 36)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "EUREKA RESIDENTIAL "
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.PictureBox1)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel4.Location = New System.Drawing.Point(994, 0)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(141, 122)
+        Me.Panel4.TabIndex = 0
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(141, 122)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Transparent
+        Me.Panel3.Controls.Add(Me.TextBox1)
+        Me.Panel3.Controls.Add(Me.txtNumber2)
+        Me.Panel3.Controls.Add(Me.txtNumber1)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel3.Location = New System.Drawing.Point(0, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(1135, 643)
+        Me.Panel3.TabIndex = 9
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(794, 599)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Visible = False
+        '
+        'txtNumber2
+        '
+        Me.txtNumber2.BackColor = System.Drawing.Color.Black
+        Me.txtNumber2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtNumber2.Font = New System.Drawing.Font("Segoe UI", 250.0!, System.Drawing.FontStyle.Bold)
+        Me.txtNumber2.ForeColor = System.Drawing.Color.White
+        Me.txtNumber2.Location = New System.Drawing.Point(624, 136)
+        Me.txtNumber2.Name = "txtNumber2"
+        Me.txtNumber2.Size = New System.Drawing.Size(424, 444)
+        Me.txtNumber2.TabIndex = 1
+        Me.txtNumber2.Text = "0"
+        Me.txtNumber2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtNumber1
+        '
+        Me.txtNumber1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtNumber1.Font = New System.Drawing.Font("Segoe UI", 250.0!, System.Drawing.FontStyle.Bold)
+        Me.txtNumber1.Location = New System.Drawing.Point(186, 136)
+        Me.txtNumber1.Name = "txtNumber1"
+        Me.txtNumber1.Size = New System.Drawing.Size(424, 444)
+        Me.txtNumber1.TabIndex = 2
+        Me.txtNumber1.Text = "0"
+        Me.txtNumber1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Panel8
+        '
+        Me.Panel8.BackColor = System.Drawing.Color.Transparent
+        Me.Panel8.Controls.Add(Me.Panel5)
+        Me.Panel8.Controls.Add(Me.Panel7)
+        Me.Panel8.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel8.Location = New System.Drawing.Point(0, 643)
+        Me.Panel8.Name = "Panel8"
+        Me.Panel8.Size = New System.Drawing.Size(1135, 58)
+        Me.Panel8.TabIndex = 8
+        '
+        'Panel5
+        '
+        Me.Panel5.Controls.Add(Me.Label1)
+        Me.Panel5.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel5.Location = New System.Drawing.Point(845, 0)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(290, 58)
+        Me.Panel5.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Monotype Corsiva", 24.0!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(11, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(284, 39)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "LOTTERY SYSTEM "
+        '
+        'Panel7
+        '
+        Me.Panel7.Controls.Add(Me.btnExit)
+        Me.Panel7.Controls.Add(Me.btnLuckyDraw)
+        Me.Panel7.Location = New System.Drawing.Point(0, 0)
+        Me.Panel7.Name = "Panel7"
+        Me.Panel7.Size = New System.Drawing.Size(485, 100)
+        Me.Panel7.TabIndex = 4
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnExit.Font = New System.Drawing.Font("Lucida Handwriting", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Image = CType(resources.GetObject("btnExit.Image"), System.Drawing.Image)
+        Me.btnExit.Location = New System.Drawing.Point(223, 14)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(98, 39)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnLuckyDraw
+        '
+        Me.btnLuckyDraw.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.btnLuckyDraw.Font = New System.Drawing.Font("Lucida Handwriting", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLuckyDraw.Image = CType(resources.GetObject("btnLuckyDraw.Image"), System.Drawing.Image)
+        Me.btnLuckyDraw.Location = New System.Drawing.Point(12, 14)
+        Me.btnLuckyDraw.Name = "btnLuckyDraw"
+        Me.btnLuckyDraw.Size = New System.Drawing.Size(205, 38)
+        Me.btnLuckyDraw.TabIndex = 3
+        Me.btnLuckyDraw.Text = "Lucky Student"
+        Me.btnLuckyDraw.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnLuckyDraw.UseVisualStyleBackColor = False
+        '
+        'Timer3
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1135, 701)
+        Me.Controls.Add(Me.Panel1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!, System.Drawing.FontStyle.Bold)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(5)
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel6.ResumeLayout(False)
+        Me.Panel6.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel8.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        Me.Panel7.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Panel6 As System.Windows.Forms.Panel
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents txtNumber2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNumber1 As System.Windows.Forms.TextBox
+    Friend WithEvents Panel8 As System.Windows.Forms.Panel
+    Friend WithEvents Panel5 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel7 As System.Windows.Forms.Panel
+    Friend WithEvents btnLuckyDraw As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ LotterySystem/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,128 @@
+﻿
+Public Class Form1
+
+    Dim count As Integer
+    Dim count2, count3 As Integer
+    Dim num, num2 As Integer
+    Dim flag As Boolean = False
+    Dim robot = CreateObject("SAPI.spvoice")
+
+
+    Private Sub btnLuckyDraw_Click(sender As Object, e As EventArgs) Handles btnLuckyDraw.Click
+        Timer1.Enabled = True
+        Timer2.Enabled = True
+        Timer3.Enabled = False
+
+        'My.Computer.Audio.PlaySystemSound(Media.SystemSounds.Asterisk)
+        flag = False
+        count = 0
+        count2 = 0
+        count3 = 0
+        Timer2.Interval = 10
+        Timer1.Interval = 100
+    End Sub
+
+    Private Sub btnLuckyDraw_MouseEnter(sender As Object, e As EventArgs) Handles btnLuckyDraw.MouseEnter
+        btnLuckyDraw.BackColor = Color.Black
+        btnLuckyDraw.ForeColor = Color.White
+    End Sub
+
+    Private Sub btnLuckyDraw_MouseLeave(sender As Object, e As EventArgs) Handles btnLuckyDraw.MouseLeave
+        btnLuckyDraw.BackColor = Color.LightGray
+        btnLuckyDraw.ForeColor = Color.Black
+    End Sub
+
+
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        count = count + 1
+        num = CInt(Rnd() * 8)
+        txtNumber1.Text = num
+
+        If count = 200 Then
+
+            Timer1.Enabled = False
+        ElseIf count = 150 Then
+            Timer1.Interval = Timer1.Interval + 4
+        ElseIf count = 190 Then
+            Timer1.Interval = Timer1.Interval + 7
+
+        End If
+
+
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        count2 = count2 + 1
+        num2 = CInt(Rnd() * 9)
+        txtNumber2.Text = num2
+
+
+        If count2 = 1500 Then
+
+            Timer2.Enabled = False
+
+            Timer3.Enabled = True
+
+            TextBox1.Text = txtNumber1.Text & txtNumber2.Text
+        ElseIf count2 = 1450 Then
+
+            Timer2.Interval = Timer2.Interval + 50
+
+        ElseIf count2 = 1480 Then
+
+            Timer2.Interval = Timer2.Interval + 70
+
+        End If
+
+
+        'If txtNumber2.Text = num2 Then
+
+        '    robot.speak("Here Is The Lucky Student")
+
+        'End 
+
+
+
+
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+   
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        count3 = count3 + 1
+        If count3 = 3 Then
+            Timer3.Enabled = False
+            robot.speak("Here Is The Lucky Student " & TextBox1.Text)
+            robot.speak("Congratulations Student Number" & TextBox1.Text)
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim ask As DialogResult
+        ask = MsgBox("Do You Want To Exit?", vbYesNo, "Exit")
+        If ask = vbYes Then
+            End
+
+        End If
+    End Sub
+
+    Private Sub btnExit_MouseEnter(sender As Object, e As EventArgs) Handles btnExit.MouseEnter
+        btnExit.BackColor = Color.Black
+        btnExit.ForeColor = Color.White
+    End Sub
+
+    Private Sub btnExit_MouseLeave(sender As Object, e As EventArgs) Handles btnExit.MouseLeave
+        btnExit.BackColor = Color.LightGray
+        btnExit.ForeColor = Color.Black
+    End Sub
+End Class
+
